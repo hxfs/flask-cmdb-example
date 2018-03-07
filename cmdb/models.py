ef489d51d33d11020d06e81eaddc2154ab8a854e
wangqj <wangqj@ushareit.com>
2018-03-07 19:29:14 +0800

    perfect page

--- cmdb/models.py
+++ cmdb/models.py
@@ -2,17 +2,18 @@
 
 from flask_sqlalchemy import SQLAlchemy
 
-
 db = SQLAlchemy()
 
 
 class User(db.Model):
-    __tablename__ = 'userinfo'
+    __tablename__ = 'opm_user'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     username = db.Column(db.String(50), nullable=False, unique=True)
     password = db.Column(db.String(50), nullable=False)
-    message = db.Column(db.String(200))
     email = db.Column(db.String(40), nullable=False)
+    create_time = db.Column(db.DateTime, nullable=False)
+    group_name = db.Column(db.String(50), nullable=False)
+    message_num = db.Column(db.Integer)
 
     def __init__(self, username, password, message, email):
         self.username = username
@@ -22,3 +23,67 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User %r>' % self.username
+
+
+class Group(db.Model):
+    __tablename__ = 'opm_group'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    groupname = db.Column(db.String(50), nullable=False, unique=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('opm_user.id'))
+
+    def __repr__(self, groupname):
+        return '<Group %r>' % self.groupname
+
+
+class Message_status(db.Model):
+    __tablename__ = 'opm_message_status'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    send_user = db.Column(db.String(50), nullable=False, unique=True)
+    recv_user = db.Column(db.String(50), nullable=False, unique=True)
+    send_time = db.Column(db.DateTime, nullable=False)
+    status = db.Column(db.Integer, nullable=False)
+
+    def __init__(self, send_user, recv_user, send_time, send_text):
+        self.send_user = send_user
+        self.recv_user = recv_user
+        self.send_time = send_time
+        self.send_text = send_text
+
+    def __repr__(self, send_user):
+        return '<Message %r>' % self.send_user
+
+
+class Ticket_type(db.Model):
+    __tablename__ = 'opm_ticket_type'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    type_name = db.Column(db.String(50), nullable=False, unique=True)
+
+    def __init__(self, type_name):
+        self.type_name = type_name
+
+    def __repr__(self, type_name):
+        return '<Ticket_type %r>' % self.type_name
+
+
+class Ticket(db.Model):
+    __tablename__ = 'opm_ticket'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    ticket_sn = db.Column(db.String(20), nullable=False, unique=True)
+    ticket_type = db.Column(
+        db.String(50), db.ForeignKey('opm_ticket_type.type_name'))
+    ticket_name = db.Column(db.String(50), nullable=False)
+    create_user = db.Column(db.String(50), nullable=False)
+    recv_user = db.Column(db.String(50), nullable=False)
+    ticket_text = db.Column(db.String(255), nullable=False)
+
+    def __init__(self, ticket_sn, ticket_type, ticket_name, create_user,
+                 recv_user, ticket_text):
+        self.ticket_sn = ticket_sn
+        self.ticket_type = ticket_type
+        self.ticket_name = ticket_name
+        self.create_user = create_user
+        self.recv_user = recv_user
+        self.ticket_text = ticket_text
+
+    def __repr__(self, send_user):
+        return '<Message %r>' % self.send_user
--- cmdb/views.py
+++ cmdb/views.py
@@ -1,19 +1,28 @@
 # _*_ coding: utf-8 _*_
 from flask import render_template, request, session, redirect, url_for
+from functools import wraps
 from models import User
 import json
 
 
 def register_view(app):
+    def check_login(func):
+        @wraps(func)
+        def wrapper(*args, **kw):
+            if 'login' not in request.full_path:
+                if 'username' in session:
+                    username = session['username']
+                    data = {'username': username}
+                    return func(*args, **data)
+                else:
+                    return redirect(url_for('login'))
+        return wrapper
+
     @app.route('/')
     @app.route('/index')
-    def index():
-        if 'username' in session:
-            username = session['username']
-            data = {"username": username}
-            return render_template("index.html", **locals())
-        else:
-            return redirect(url_for('login'))
+    @check_login
+    def index(*args, **kw):
+        return render_template('index.html', **locals())
 
     @app.route('/login', methods=['GET', 'POST'])
     def login():
@@ -56,6 +65,31 @@ def register_view(app):
             return json.dumps(messages)
         else:
             return "null"
+
+    @app.route('/vpnuser')
+    @check_login
+    def vpnuser(*args, **kw):
+        return render_template('vpn_user.html', **locals())
+
+    @app.route('/jumpuser')
+    @check_login
+    def jumpuser(*args, **kw):
+        return render_template('jump_user.html', **locals())
+
+    @app.route('/groupname')
+    @check_login
+    def groupname(*args, **kw):
+        return render_template('group.html', **locals())
+
+    @app.route('/opm_user')
+    @check_login
+    def opm_user(*args, **kw):
+        return render_template("opm_user.html", **locals())
+
+    @app.route('/iamuser')
+    @check_login
+    def iamuser(*args, **kw):
+        return render_template("iam.html", **locals())
 
     @app.route('/test', methods=['GET', 'POST'])
     def test():
